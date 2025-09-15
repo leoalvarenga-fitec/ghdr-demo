cea075a2bd36fa16bfc6534bb3f6740e644590c5
Leonardo A. Alvarenga <lasilva@fitec.org.br>
2025-09-15 16:25:56 -0300

    fix: typo

--- hello.sh
+++ hello.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh:
 
-echo "If you are seeing this, it means this Container is working and its image was properly build and published! Congrats!"
+echo "If you are seeing this, it means this Container is working and its image was properly built and published! Congrats!"
 
